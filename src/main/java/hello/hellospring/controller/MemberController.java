e8ce18b830b5a3bf590cacd30e897d45ae0253ff
minsng <tjdalstmd033@naver.com>
2023-05-08 15:24:58 +0900

    0508

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,10 +7,24 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
+    //필드로 di 주입(비추천)
+    //@Autowired private MemberService memberService;
+
+    // setter로 di 주입
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+    // 생성자로 di 주입 (권장)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+
+
+
 }
